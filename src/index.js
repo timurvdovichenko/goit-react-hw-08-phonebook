0ef91f4c1d37d5dbfe0a4547d087375c5212eee1
Timur.Vdovichenko <timur.vdovichenko@gmail.com>
2023-07-17 01:25:07 +0300

    fix basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/goit-js-hw-08-phonebook">
+        <BrowserRouter basename="/ggoit-react-hw-08-phonebook">
           <App />
         </BrowserRouter>
       </PersistGate>
